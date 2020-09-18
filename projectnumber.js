4ae1b83c3e0281e52e691feff771ead52a15cf33
brendan802 <67173637+brendan802@users.noreply.github.com>
2020-09-18 14:37:11 -0400

    needed a victory message

--- projectnumber.js
+++ projectnumber.js
@@ -10,32 +10,42 @@ function ask(questionText) {
 async function start() {
     let minValue=1
     let maxValue=100
-    let question = await ask("Let's play a game, do you have a number between 1-100? ")
-
+    let question = await ask("Let's play a game, are you thinking of a number between 1-100? ")
+    
+    
     while (question === 'no') {
-        question = await ask('then get one')
+        question = await ask('Then you should get one please, come back when you do.')
     }
+    
     while (question === 'yes') {
         let guess= Math.floor((maxValue+minValue) / 2)
-        let first = await ask('is your number greater or less than ' + guess + '? ')
+        {let first = await ask('Is your number greater, less than , or equal to' + ' ' + guess + '? ')
+        if (first==='equal to'){
+            response=await ask ("Huzahhhh! I am, and always will be the best.")
+        }
+        
+        
         //let second = await ask('Is your number greater or less than 75?')
         console.log('Hmmm...' + first + " " + "you say.")
+    
 
         if (first === 'greater') {
             minValue= guess
-          //  first = await ask('Is your number greater or less than ' + guess + '?')
+            
+            //  first = await ask('Is your number greater or less than ' + guess + '?')
 
         }
 
         if (first === 'less than') 
             maxValue=guess
+            
           //  first = await ask('is your number greater or less than ' + guess + '?')
            
         }
         
 
 
-    }
+    }}
 
 start()
 
